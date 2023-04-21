c0208ffda78c49f67239101b2425049a949be652
LordGumba <rou21004@byui.edu>
2023-04-21 16:42:35 -0600

    kjtg

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "F";
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            Console.Write("You passed! Great Job! ");
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            Console.Write("Your hard work paid off, You passed!");
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            Console.Write("You Passed! But just barely.");
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            Console.Write("You failed.");
+            letter = "D";
+        }
+        else
+        {
+            Console.Write("...I'm disapointed in you.");
+            letter = "F";
+        }
+        
+        Console.WriteLine($"You got a {letter}.");
+        
     }
 }
