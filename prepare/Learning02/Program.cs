e8970318942616dd96eef50b8d5e57928d0a825c
LordGumba <rou21004@byui.edu>
2023-05-02 11:07:55 -0600

    gfds

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,27 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Clown";
+        job1._company = "McDonalds";
+        job1._startYear = 2016;
+        job1._endYear = 2102;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Hamburgular";
+        job2._company = "McJerusalem";
+        job2._startYear = 0022;
+        job2._endYear = 1623;
+
+        Resume myResume = new Resume();
+        myResume._name = "Percy Jackson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
