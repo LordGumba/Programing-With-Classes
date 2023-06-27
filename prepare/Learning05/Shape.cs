7b6bdefc9658d6428c9af0ebdb7871c977e4b451
LordGumba <rou21004@byui.edu>
2023-06-26 18:28:50 -0600

    bef

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,20 @@
+
+
+class Shapes
+{
+    private string _color;
+
+    public Shapes(string color)
+    {
+        _color = color;
+    }
+    public string getColor()
+    {
+        return _color;
+    }
+    public void setColor(string color)
+    {
+        _color = color;
+    }
+    public abstract double GetArea();
+}
