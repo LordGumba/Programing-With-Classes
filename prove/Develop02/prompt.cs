a92fce8f24aa16e49542afdb296e2630fa2090ce
LordGumba <rou21004@byui.edu>
2023-07-01 18:09:28 -0600

    jtd

--- prove/Develop02/prompt.cs
+++ prove/Develop02/prompt.cs
@@ -3,19 +3,21 @@ using System;
 
 namespace Develop02
 {
-    public class prompt{
+    public class Prompt{
         Random rnd = new Random();
         public List<string> prompt;
         
-        public prompt(){
+        public Prompt(){
             prompt.Add("What was a highlight of our day?");
             prompt.Add("Have you met anyone you would want to strangle today?");
             prompt.Add("Did you touch grass today?");
             prompt.Add("What was the hardest task you have accomplished today?");
 
         }
-        public string GetRandomPrompt(){
-            
+        public string GetRandomPrompt()
+        {
+            int index = rnd.Next(prompt.Count);
+            return prompt[index];
         }
     }
 }
